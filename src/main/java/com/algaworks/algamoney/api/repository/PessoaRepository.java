7b484c994d79a5f86019af7f4791fde788e22264
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-01-20 18:33:43 -0300

    Adicionando paginação na busca de pessoas

--- src/main/java/com/algaworks/algamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/algaworks/algamoney/api/repository/PessoaRepository.java
@@ -2,8 +2,12 @@ package com.algaworks.algamoney.api.repository;
 
 import com.algaworks.algamoney.api.model.Pessoa;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
-    
+    public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 }
