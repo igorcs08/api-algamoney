8eedbd61947086bd82eaed2a62fdffe99e6075ab
Igor Cavalcante <igor.cavalcante96@gmail.com>
2022-01-20 15:06:05 -0300

    Alterando expiração do AccessToken

--- src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -12,6 +12,7 @@ import com.algaworks.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Er
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
 import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
+import com.algaworks.algamoney.api.repository.projection.ResumoLancamento;
 import com.algaworks.algamoney.api.service.LancamentoService;
 import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -23,6 +24,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,51 +39,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/lancamentos")
 public class LancamentoResource {
 
-    @Autowired
-    private LancamentoRepository lancamentoRepository;
+  @Autowired
+  private LancamentoRepository lancamentoRepository;
 
-    @Autowired
-    private LancamentoService lancamentoService;
+  @Autowired
+  private LancamentoService lancamentoService;
 
-    @Autowired
-    private ApplicationEventPublisher publisher;
+  @Autowired
+  private ApplicationEventPublisher publisher;
 
-    @Autowired
-    private MessageSource messageSource;
+  @Autowired
+  private MessageSource messageSource;
 
-    @GetMapping
-    public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
-        return lancamentoRepository.filtrar(lancamentoFilter, pageable);
-    }
+  @GetMapping
+  public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+    return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+  }
 
-    @GetMapping("/{codigo}")
-    public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
-        Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
-        return !lancamento.isEmpty() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
-    }
+  @GetMapping(params = "resumo")
+  @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and hasAuthority('SCOPE_read')")
+  public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+    return lancamentoRepository.resumir(lancamentoFilter, pageable);
+  }
 
-    @PostMapping
-    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-        Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
+  @GetMapping("/{codigo}")
+  public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
+    Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
+    return !lancamento.isEmpty() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
+  }
 
-        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+  @PostMapping
+  public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+    Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
-    }
+    publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 
-    @DeleteMapping("/{codigo}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void remover(@PathVariable Long codigo) {
-        this.lancamentoRepository.deleteById(codigo);
-    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+  }
 
-    @ExceptionHandler({ PessoaInexistenteOuInativaException.class })
-    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
-        String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-inativa", null,
-                LocaleContextHolder.getLocale());
-        String mensagemDesenvolvedor = ex.toString();
-        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-        return ResponseEntity.badRequest().body(erros);
-    }
+  @DeleteMapping("/{codigo}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void remover(@PathVariable Long codigo) {
+    this.lancamentoRepository.deleteById(codigo);
+  }
+
+  @ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+  public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+    String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-inativa", null,
+        LocaleContextHolder.getLocale());
+    String mensagemDesenvolvedor = ex.toString();
+    List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+    return ResponseEntity.badRequest().body(erros);
+  }
 
 }
